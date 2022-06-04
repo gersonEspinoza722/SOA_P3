fe4ff90520c2056d399b7e1ac2797a27af5c6056
gersonEspinoza722 <gerespriv@gmail.com>
2022-06-04 12:36:24 -0600

    cars update

--- src/interfaz.c
+++ src/interfaz.c
@@ -98,40 +98,40 @@ void cargar_interfaz() {
     insertar_dict(BUSNARANJAATRAS, crear_item("images/ORANGE_BB.png", BUSNARANJAATRAS, 0.065, 0.020), hashImagenes, TAMANIO);
     insertar_dict(BUSNARANJAFRENTE, crear_item("images/ORANGE_BF.png", BUSNARANJAFRENTE, 0.065, 0.020), hashImagenes, TAMANIO);
     //Ambulancia
-    insertar_dict(AMBULANCIAIZQ, crear_item("images/AMBULANCEL.png", AMBULANCIAIZQ, 0.025, 0.015), hashImagenes, TAMANIO);
-    insertar_dict(AMBULANCIADER, crear_item("images/AMBULANCER.png", AMBULANCIADER, 0.025, 0.015), hashImagenes, TAMANIO);
-    insertar_dict(AMBULANCIAATRAS, crear_item("images/AMBULANCEB.png", AMBULANCIAATRAS, 0.025, 0.015), hashImagenes, TAMANIO);
-    insertar_dict(AMBULANCIAFRENTE, crear_item("images/AMBULANCEF.png", AMBULANCIAFRENTE, 0.025, 0.015), hashImagenes, TAMANIO);
+    insertar_dict(AMBULANCIAIZQ, crear_item("images/AMBULANCEL.png", AMBULANCIAIZQ, 0.035, 0.025), hashImagenes, TAMANIO);
+    insertar_dict(AMBULANCIADER, crear_item("images/AMBULANCER.png", AMBULANCIADER, 0.035, 0.025), hashImagenes, TAMANIO);
+    insertar_dict(AMBULANCIAATRAS, crear_item("images/AMBULANCEB.png", AMBULANCIAATRAS, 0.035, 0.025), hashImagenes, TAMANIO);
+    insertar_dict(AMBULANCIAFRENTE, crear_item("images/AMBULANCEF.png", AMBULANCIAFRENTE, 0.035, 0.025), hashImagenes, TAMANIO);
     //Carro rojo
-    insertar_dict(CARROROJOIZQ, crear_item("images/REDL.jpg", CARROROJOIZQ, 0.025, 0.015), hashImagenes, TAMANIO);
-    insertar_dict(CARROROJODER, crear_item("images/REDR.jpg", CARROROJODER, 0.025, 0.015), hashImagenes, TAMANIO);
-    insertar_dict(CARROROJOATRAS, crear_item("images/REDB.jpg", CARROROJOATRAS, 0.025, 0.015), hashImagenes, TAMANIO);
-    insertar_dict(CARROROJOFRENTE, crear_item("images/REDF.jpg", CARROROJOFRENTE, 0.025, 0.015), hashImagenes, TAMANIO);
+    insertar_dict(CARROROJOIZQ, crear_item("images/REDL.png", CARROROJOIZQ, 0.035, 0.025), hashImagenes, TAMANIO);
+    insertar_dict(CARROROJODER, crear_item("images/REDR.png", CARROROJODER, 0.035, 0.025), hashImagenes, TAMANIO);
+    insertar_dict(CARROROJOATRAS, crear_item("images/REDB.png", CARROROJOATRAS, 0.035, 0.025), hashImagenes, TAMANIO);
+    insertar_dict(CARROROJOFRENTE, crear_item("images/REDF.png", CARROROJOFRENTE, 0.035, 0.025), hashImagenes, TAMANIO);
     //Carro verde    
     insertar_dict(CARROVERDEIZQ, crear_item("images/GREENL.png", CARROVERDEIZQ, 0.035, 0.025), hashImagenes, TAMANIO);
     insertar_dict(CARROVERDEDER, crear_item("images/GREENR.png", CARROVERDEDER, 0.035, 0.025), hashImagenes, TAMANIO);
     insertar_dict(CARROVERDEATRAS, crear_item("images/GREENB.png", CARROVERDEATRAS, 0.035, 0.025), hashImagenes, TAMANIO);
     insertar_dict(CARROVERDEFRENTE, crear_item("images/GREENF.png", CARROVERDEFRENTE, 0.035, 0.025), hashImagenes, TAMANIO);
     //Carro azul
-    insertar_dict(CARROAZULIZQ, crear_item("images/BLUEL.jpg", CARROAZULIZQ, 0.025, 0.015), hashImagenes, TAMANIO);
-    insertar_dict(CARROAZULDER, crear_item("images/BLUER.jpg", CARROAZULDER, 0.025, 0.015), hashImagenes, TAMANIO);
-    insertar_dict(CARROAZULATRAS, crear_item("images/BLUEB.jpg", CARROAZULATRAS, 0.025, 0.015), hashImagenes, TAMANIO);
-    insertar_dict(CARROAZULFRENTE, crear_item("images/BLUEF.jpg", CARROAZULFRENTE, 0.025, 0.015), hashImagenes, TAMANIO);
+    insertar_dict(CARROAZULIZQ, crear_item("images/BLUEL.png", CARROAZULIZQ, 0.035, 0.025), hashImagenes, TAMANIO);
+    insertar_dict(CARROAZULDER, crear_item("images/BLUER.png", CARROAZULDER, 0.035, 0.025), hashImagenes, TAMANIO);
+    insertar_dict(CARROAZULATRAS, crear_item("images/BLUEB.png", CARROAZULATRAS, 0.035, 0.025), hashImagenes, TAMANIO);
+    insertar_dict(CARROAZULFRENTE, crear_item("images/BLUEF.png", CARROAZULFRENTE, 0.035, 0.025), hashImagenes, TAMANIO);
     //Carro blanco
-    insertar_dict(CARROBLANCOIZQ, crear_item("images/WHITEL.jpg", CARROBLANCOIZQ, 0.025, 0.015), hashImagenes, TAMANIO);
-    insertar_dict(CARROBLANCODER, crear_item("images/WHITER.jpg", CARROBLANCODER, 0.025, 0.015), hashImagenes, TAMANIO);
-    insertar_dict(CARROBLANCOATRAS, crear_item("images/WHITEB.jpg", CARROBLANCOATRAS, 0.025, 0.015), hashImagenes, TAMANIO);
-    insertar_dict(CARROBLANCOFRENTE, crear_item("images/WHITEF.jpg", CARROBLANCOFRENTE, 0.025, 0.015), hashImagenes, TAMANIO);
+    insertar_dict(CARROBLANCOIZQ, crear_item("images/WHITEL.png", CARROBLANCOIZQ, 0.035, 0.025), hashImagenes, TAMANIO);
+    insertar_dict(CARROBLANCODER, crear_item("images/WHITER.png", CARROBLANCODER, 0.035, 0.025), hashImagenes, TAMANIO);
+    insertar_dict(CARROBLANCOATRAS, crear_item("images/WHITEB.png", CARROBLANCOATRAS, 0.035, 0.025), hashImagenes, TAMANIO);
+    insertar_dict(CARROBLANCOFRENTE, crear_item("images/WHITEF.png", CARROBLANCOFRENTE, 0.035, 0.025), hashImagenes, TAMANIO);
     //Carro negro   
-    insertar_dict(CARRONEGROIZQ, crear_item("images/BLACKL.jpg", CARRONEGROIZQ, 0.025, 0.015), hashImagenes, TAMANIO);
-    insertar_dict(CARRONEGRODER, crear_item("images/BLACKR.jpg", CARRONEGRODER, 0.025, 0.015), hashImagenes, TAMANIO);
-    insertar_dict(CARRONEGROATRAS, crear_item("images/BLACKB.jpg", CARRONEGROATRAS, 0.025, 0.015), hashImagenes, TAMANIO);
-    insertar_dict(CARRONEGROFRENTE, crear_item("images/BLACKF.jpg", CARRONEGROFRENTE, 0.025, 0.015), hashImagenes, TAMANIO);
+    insertar_dict(CARRONEGROIZQ, crear_item("images/BLACKL.png", CARRONEGROIZQ, 0.035, 0.025), hashImagenes, TAMANIO);
+    insertar_dict(CARRONEGRODER, crear_item("images/BLACKR.png", CARRONEGRODER, 0.035, 0.025), hashImagenes, TAMANIO);
+    insertar_dict(CARRONEGROATRAS, crear_item("images/BLACKB.png", CARRONEGROATRAS, 0.035, 0.025), hashImagenes, TAMANIO);
+    insertar_dict(CARRONEGROFRENTE, crear_item("images/BLACKF.png", CARRONEGROFRENTE, 0.035, 0.025), hashImagenes, TAMANIO);
     //Carro amarillo
-    insertar_dict(CARROAMARILLOIZQ, crear_item("images/YELLOWL.jpg", CARROAMARILLOIZQ, 0.025, 0.015), hashImagenes, TAMANIO);
-    insertar_dict(CARROAMARILLODER, crear_item("images/YELLOWR.jpg", CARROAMARILLODER, 0.025, 0.015), hashImagenes, TAMANIO);
-    insertar_dict(CARROAMARILLOATRAS, crear_item("images/YELLOWB.jpg", CARROAMARILLOATRAS, 0.025, 0.015), hashImagenes, TAMANIO);
-    insertar_dict(CARROAMARILLOFRENTE, crear_item("images/YELLOWF.jpg", CARROAMARILLOFRENTE, 0.025, 0.015), hashImagenes, TAMANIO);
+    insertar_dict(CARROAMARILLOIZQ, crear_item("images/YELLOWL.png", CARROAMARILLOIZQ, 0.035, 0.025), hashImagenes, TAMANIO);
+    insertar_dict(CARROAMARILLODER, crear_item("images/YELLOWR.png", CARROAMARILLODER, 0.035, 0.025), hashImagenes, TAMANIO);
+    insertar_dict(CARROAMARILLOATRAS, crear_item("images/YELLOWB.png", CARROAMARILLOATRAS, 0.035, 0.025), hashImagenes, TAMANIO);
+    insertar_dict(CARROAMARILLOFRENTE, crear_item("images/YELLOWF.png", CARROAMARILLOFRENTE, 0.035, 0.025), hashImagenes, TAMANIO);
 
     if (pthread_mutex_init(&lockVehiculo, NULL) != 0) {
         printf("\n Init del mutex del vehiculo fallo\n");
